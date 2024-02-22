6271b5adc9e2007b41140fd98ff639d163d6d1ca
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-02-21 22:43:30 -0800

    add flashing effect to window

--- Window.cpp
+++ Window.cpp
@@ -17,12 +17,26 @@ bool Window::init(unsigned int width, unsigned int height, std::string title) {
 		glfwTerminate();
 		return false;
 	}
+
+	glfwMakeContextCurrent(mWindow);
+
 	Logger::log(1, "%s: Window successfully initialized\n", __FUNCTION__);
 	return true;
 }
 
 void Window::mainLoop() {
+	glfwSwapInterval(1);
+	float color = 0.0f;
+
 	while (!glfwWindowShouldClose(mWindow)) {
+
+		color >= 1.0f ? color = 0.0f : color += 0.01f;
+		glClearColor(color, color, color, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// swap buffers
+		glfwSwapBuffers(mWindow);
+
 		//poll events in a loop
 		glfwPollEvents();
 	}
