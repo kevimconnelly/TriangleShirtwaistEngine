0bb9c9e637484ae7457039636177806c59e039b1
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-02-24 00:28:40 -0800

    check vulkan windows and devices

--- Window.cpp
+++ Window.cpp
@@ -1,6 +1,7 @@
 #include "Window.h"
 #include "../TriangleShirtwaistEngine/Logger.h"
 #include "vulkan/vulkan.h"
+#include "vector"
 
 bool Window::init(unsigned int width, unsigned int height, std::string title) {
 	if (!glfwInit()) {
@@ -62,7 +63,7 @@ void Window::cleanup() {
 }
 
 bool Window::initVulkan() {
-	VkResult result = VK_ERROR_UNKNOWN;
+	/*VkResult result = VK_ERROR_UNKNOWN;
 
 	VkApplicationInfo mAppInfo{};
 	mAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -75,7 +76,6 @@ bool Window::initVulkan() {
 		Logger::log(1, "%s error: no Vulkan extensions found\n", __FUNCTION__);
 		return false;
 	}
-
 
 	mAppInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 1, 0);
 
@@ -92,4 +92,81 @@ bool Window::initVulkan() {
 		Logger::log(1, "%s: Could not create Instance (%i)\n" __FUNCTION__, result);
 		return false;
 	}
+
+	uint32_t physicalDeviceCount = 0;
+	vkEnumeratePhysicalDevices(mInstance, &physicalDeviceCount, nullptr);
+
+	if (physicalDeviceCount == 0) {
+		Logger::log(1, "%s: No Vulkan capable GPU found\n", __FUNCTION__);
+		return false;
+	}
+
+	std::vector<VkPhysicalDevice> devices;
+	vkEnumeratePhysicalDevices(mInstance, &physicalDeviceCount, devices.data());
+
+	result = glfwCreateWindowSurface(mInstance, mWindow, nullptr, &mSurface);
+	if (result != VK_SUCCESS) {
+		Logger::log(1, "%s: Could not create Vulkan surface\n", __FUNCTION__);
+		return false;
+	}*/
+	VkResult result = VK_ERROR_UNKNOWN;
+
+	VkApplicationInfo mAppInfo{};
+	mAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	mAppInfo.pNext = nullptr;
+	mAppInfo.pApplicationName = mApplicationName.c_str();
+	mAppInfo.applicationVersion = VK_MAKE_API_VERSION(0, 0, 0, 1);
+	mAppInfo.pEngineName = "Game Animations Programming";
+	mAppInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+	mAppInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 1, 0);
+
+	uint32_t extensionCount = 0;
+	const char** extensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+
+	if (extensionCount == 0) {
+		Logger::log(1, "%s error: no Vulkan extensions found, need at least 'VK_KHR_surface'\n", __FUNCTION__);
+		return false;
+	}
+
+	Logger::log(1, "%s: Found %u Vulkan extensions\n", __FUNCTION__, extensionCount);
+	for (int i = 0; i < extensionCount; ++i) {
+		Logger::log(1, "%s: %s\n", __FUNCTION__, std::string(extensions[i]).c_str());
+	}
+
+	VkInstanceCreateInfo mCreateInfo{};
+	mCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	mCreateInfo.pNext = nullptr;
+	mCreateInfo.pApplicationInfo = &mAppInfo;
+	mCreateInfo.enabledExtensionCount = extensionCount;
+	mCreateInfo.ppEnabledExtensionNames = extensions;
+	mCreateInfo.enabledLayerCount = 0;
+
+	result = vkCreateInstance(&mCreateInfo, nullptr, &mInstance);
+	if (result != VK_SUCCESS) {
+		Logger::log(1, "%s: Could not create Vulkan instance (%i)\n", __FUNCTION__, result);
+		return false;
+	}
+
+	uint32_t physicalDeviceCount = 0;
+	vkEnumeratePhysicalDevices(mInstance, &physicalDeviceCount, nullptr);
+
+	if (physicalDeviceCount == 0) {
+		Logger::log(1, "%s: No Vulkan capable GPU found\n", __FUNCTION__);
+		return false;
+	}
+
+	std::vector<VkPhysicalDevice> devices;
+	devices.resize(physicalDeviceCount);
+	vkEnumeratePhysicalDevices(mInstance, &physicalDeviceCount, devices.data());
+
+	Logger::log(1, "%s: Found %u physical device(s)\n", __FUNCTION__, physicalDeviceCount);
+
+	result = glfwCreateWindowSurface(mInstance, mWindow, nullptr, &mSurface);
+	if (result != VK_SUCCESS) {
+		Logger::log(1, "%s: Could not create Vulkan surface\n", __FUNCTION__);
+		return false;
+	}
+
+	return true;
+
 }
