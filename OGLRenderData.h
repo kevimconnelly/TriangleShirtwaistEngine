a4d7c9a872a1996f1acadbf3d472033d80363992
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-02-27 22:02:23 -0800

    add render data header

--- OGLRenderData.h
+++ OGLRenderData.h
@@ -1,2 +1,14 @@
+/* OpenGL */
 #pragma once
+#include <vector>
 
+#include <glm/glm.hpp>
+
+struct OGLVertex {
+	glm::vec3 position;
+	glm::vec2 uv;
+};
+
+struct OGLMesh {
+	std::vector<OGLVertex> vertices;
+};
