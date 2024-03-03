d8648e7ce33a901c053bb84da29b8c96cce702bb
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-03-03 14:42:22 -0800

    update ogl renderer

--- OGLRenderer.cpp
+++ OGLRenderer.cpp
@@ -2,60 +2,91 @@
 #include "Logger.h"
 
 bool OGLRenderer::init(unsigned int width, unsigned int height) {
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		return false;
-	}
 
-	if (!GLAD_GL_VERSION_4_6) {
-		return false;
-	}
+    /* initalize GLAD */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        Logger::log(1, "%s error: failed to initialize GLAD\n", __FUNCTION__);
+        return false;
+    }
 
-	if (!mFrameBuffer.init(width, height)) {
-		return false;
-	}
+    if (!GLAD_GL_VERSION_4_6) {
+        Logger::log(1, "%s error: failed to get at least OpenGL 4.6\n", __FUNCTION__);
+        return false;
+    }
 
-	if (!mTex.loadTexture("texture/crate.png")) {
-		return false;
-	}
-	MVertexBuffer.init();
+    GLint majorVersion, minorVersion;
+    glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+    glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+    Logger::log(1, "%s: OpenGL %d.%d initializeed\n", __FUNCTION__, majorVersion, minorVersion);
 
-	if (!mShader.loadShaders("shader/basic.vert", "shader/basic.frag")) {
-		return false;
-	}
+    if (!mFramebuffer.init(width, height)) {
+        Logger::log(1, "%s error: could not init Framebuffer\n", __FUNCTION__);
+        return false;
+    }
+    Logger::log(1, "%s: framebuffer succesfully initialized\n", __FUNCTION__);
 
-	return true;
+    if (!mTex.loadTexture("textures/crate.png")) {
+        Logger::log(1, "%s: texture loading failed\n", __FUNCTION__);
+        return false;
+    }
+    Logger::log(1, "%s: texture successfully loaded\n", __FUNCTION__);
+
+    mVertexBuffer.init();
+    Logger::log(1, "%s: vertex buffer successfully created\n", __FUNCTION__);
+
+    if (!mShader.loadShaders("shader/basic.vert", "shader/basic.frag")) {
+        Logger::log(1, "%s: shader loading failed\n", __FUNCTION__);
+        return false;
+    }
+    Logger::log(1, "%s: shaders succesfully loaded\n", __FUNCTION__);
+
+    return true;
 }
 
 void OGLRenderer::setSize(unsigned int width, unsigned int height) {
-	mTriangleCount = vertexData.vertices.size();
-	glViewport(0, 0, width, height);
+    /* handle minimize */
+    if (width == 0 || height == 0) {
+        return;
+    }
+
+    mFramebuffer.resize(width, height);
+    glViewport(0, 0, width, height);
+
+    Logger::log(1, "%s: resized window to %dx%d\n", __FUNCTION__, width, height);
 }
 
 void OGLRenderer::uploadData(OGLMesh vertexData) {
-	mTriangleCount = vertexData.vertices.size();
-	mVertexBuffer.uploadData(vertexData);
+    mTriangleCount = vertexData.vertices.size() / 3;
+    mVertexBuffer.uploadData(vertexData);
 }
 
 void OGLRenderer::draw() {
-	mFrameBuffer.bind();
-	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_CULL_FACE);
+    /* draw to framebuffer */
+    mFramebuffer.bind();
 
-	mShader.use();
-	mTex.bind();
-	mVertexBuffer.bind();
-	mVertexBuffer.draw(GL_TRIANGLES, 0, mTriangleCount);
-	mVertexBuffer.unbind();
-	mTex.unbind();
-	mFrameBuffer.unbind();
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClearDepth(1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
 
-	mFrameBuffer.drawToScreen();
+    mShader.use();
+    mTex.bind();
+    mVertexBuffer.bind();
+
+    mVertexBuffer.draw(GL_TRIANGLES, 0, mTriangleCount * 3);
+    mVertexBuffer.unbind();
+    mTex.unbind();
+
+    mFramebuffer.unbind();
+
+    /* blit color buffer to screen */
+    mFramebuffer.drawToScreen();
 }
 
 void OGLRenderer::cleanup() {
-	mShader.cleanup();
-	mtex.cleanup();
-	mVertexBuffer.cleanup();
-	mFrameBuffer.cleanup();
+    mShader.cleanup();
+    mTex.cleanup();
+    mVertexBuffer.cleanup();
+    mFramebuffer.cleanup();
 }
